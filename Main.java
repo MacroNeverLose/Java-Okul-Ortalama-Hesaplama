4e0f78d489b079f240fe0f8ad2eebf72c84fc2dc
Mert Demir <107760530+MacroNeverLose@users.noreply.github.com>
2022-08-13 13:06:49 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,25 @@
-#Yakında
+import java.util.Scanner; 
+/// İmport Edılecekler //
+public class Okul {
+   public class static void(String[] args) {
+int mat,fiz,kim,tur,tar,muz
+  // Coklu tanımlar //
+  String;
+     Scanner scan = new Scanner(System.in);
+      
+  System.out.printeLn(“matematik notunuz giriniz“);
+     mat = scan.nextlnt();
+  System.out.printeLn(“fizik notunuz giriniz“);
+     fiz = scan.nextlnt();
+       System.out.printeLn(“kimya notunuz giriniz“);
+     kim = scan.nextlnt();
+       System.out.printeLn(“tarih notunuz giriniz“);
+     tur = scan.nextlnt();
+       System.out.printeLn(“türkçe notunuz giriniz“);
+     tar = scan.nextlnt();
+       System.out.printeLn(“müzik notunuz giriniz“);
+     muz = scan.nextlnt();
+int sonuc = (mat + fiz + kim + tur + tar + muz)
+ sonuc = (sonuc  / 6) >=60 ?
+  “sınıfı geçtiniz ; sınfta kaldınız“
+   }
